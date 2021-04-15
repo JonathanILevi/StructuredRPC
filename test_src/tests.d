a4ef82266f914d55e5fe56c2dfe5103f65286295
Jonathan <JonathanILevi@gmail.com>
2021-04-15 17:16:50 -0500

    Unittest updates.

--- test_src/tests.d
+++ test_src/tests.d
@@ -3,12 +3,13 @@ module tests;
 import structuredrpc;
 import treeserial;
 
+import std.stdio;
+import std.exception;
+import std.conv;
+
 unittest {
-	pragma(msg, "Compiling Test A");
-	import std.stdio;
-	writeln("Running Test A");
-	import std.exception;
-	import std.conv;
+	pragma(msg, "Compiling Test Receive");
+	writeln("Running Test Receive");
 	
 	enum Src;
 	
@@ -31,11 +32,8 @@ unittest {
 	assert(lastMsg == "msg: 1");
 }
 unittest {
-	pragma(msg, "Compiling Test B");
-	import std.stdio;
-	writeln("Running Test B");
-	import std.exception;
-	import std.conv;
+	pragma(msg, "Compiling Test Receive w/ Connection");
+	writeln("Running Test Receive w/ Connection");
 	
 	enum Src;
 	class Connection {}
@@ -77,11 +75,8 @@ unittest {
 	assert(lastMsg == "msg4: remote - 1");
 }
 unittest {
-	pragma(msg, "Compiling Test C");
-	import std.stdio;
-	writeln("Running Test C");
-	import std.exception;
-	import std.conv;
+	pragma(msg, "Compiling Test Split-src Receive");
+	writeln("Running Test Split-src Receive");
 	
 	enum SrcClient;
 	enum SrcServer;
@@ -115,11 +110,8 @@ unittest {
 	assert(lastMsg == "msg3: 1");
 }
 unittest {
-	pragma(msg, "Compiling Test D");
-	import std.stdio;
-	writeln("Running Test D");
-	import std.exception;
-	import std.conv;
+	pragma(msg, "Compiling Test Send");
+	writeln("Running Test Send");
 	
 	enum Src;
 	
@@ -142,11 +134,8 @@ unittest {
 	assert(lastSendData == [0,5,0,0,0]);
 }
 unittest {
-	pragma(msg, "Compiling Test E");
-	import std.stdio;
-	writeln("Running Test E");
-	import std.exception;
-	import std.conv;
+	pragma(msg, "Compiling Test Split-src Send");
+	writeln("Running Test Split-src Send");
 	
 	enum SrcClient;
 	enum SrcServer;
@@ -203,11 +192,8 @@ unittest {
 	assert(lastMsg == "msg3: 1");
 }
 unittest {
-	pragma(msg, "Compiling Test F");
-	import std.stdio;
-	writeln("Running Test F");
-	import std.exception;
-	import std.conv;
+	pragma(msg, "Compiling Test Different Send");
+	writeln("Running Test Different Send");
 	
 	enum Src;
 	class Client {}
@@ -258,6 +244,133 @@ unittest {
 	assert(lastSendData == [0, 0,5,0,0,0]);
 	s.rpcRecv!Src(new Client(), lastSendData[1..$]);
 	assert(lastMsg == "msg2: 5");
+}
+unittest {
+	pragma(msg, "Compiling Test Template-src Receive");
+	writeln("Running Test Template-src Receive");
+	
+	enum SrcA;
+	enum SrcB;
+	
+	string lastMsg = "";
+	class A {
+		@RPC!SrcA
+		@RPC!SrcB
+		void msg(Src)(int x) {
+			static if (is(Src==SrcA))
+				lastMsg = "msg: a - "~x.to!string;
+			static if (is(Src==SrcB))
+				lastMsg = "msg: b - "~x.to!string;
+		}
+		mixin MakeRPCReceive!SrcA;
+		mixin MakeRPCReceive!SrcB;
+	}
+	A a = new A;
+	a.rpcRecv!SrcA(0 ~ serialize(1));
+	assert(lastMsg == "msg: a - 1");
+	a.rpcRecv!SrcB(0 ~ serialize(1));
+	assert(lastMsg == "msg: b - 1");
+}
+unittest {
+	pragma(msg, "Compiling Test Send in Receive");
+	writeln("Running Test Send in Receive");
+	
+	enum Src;
+	
+	string lastMsg = "";
+	const(ubyte)[] lastSendData = [];
+	class A {
+		@RPCSend!Src
+		void rpcSend(const(ubyte)[] data) {
+			lastSendData = data;
+		}
+		@RPC!Src
+		void msg(int x) {
+			lastMsg = "msg: "~x.to!string;
+			msg_send!Src(5);
+		}
+		mixin MakeRPCReceive!Src;
+		mixin MakeRPCSend!Src;
+	}
+	A a = new A;
+	a.rpcRecv!Src(0 ~ serialize(1));
+	assert(lastMsg == "msg: 1");
+	assert(lastSendData == [0,5,0,0,0]);
+}
+unittest {
+	pragma(msg, "Compiling Test Send in Template Receive");
+	writeln("Running Test Send in Template Receive");
+	
+	enum Src;
+	
+	string lastMsg = "";
+	const(ubyte)[] lastSendData = [];
+	class A {
+		@RPCSend!Src
+		void rpcSend(const(ubyte)[] data) {
+			lastSendData = data;
+		}
+		@RPC!Src
+		void msg(S)(int x) {
+			lastMsg = "msg: "~x.to!string;
+			msg_send!Src(5);
+		}
+		mixin MakeRPCReceive!Src;
+		mixin MakeRPCSend!Src;
+	}
+	A a = new A;
+	a.rpcRecv!Src(0 ~ serialize(1));
+	assert(lastMsg == "msg: 1");
+	assert(lastSendData == [0,5,0,0,0]);
+}
+unittest {
+	pragma(msg, "Compiling Test Different Send in Template Receive");
+	writeln("Running Test Different Send in Template Receive");
+	
+	enum Src;
+	class Client {}
+	
+	string lastMsg = "";
+	const(ubyte)[] lastSendData = [];
+	class ClientClass() {
+		@RPC!Src
+		void msg(S)(int x) {
+			lastMsg = "msg: "~x.to!string;
+			msg2_send!Src(x+1);
+		}
+		mixin MakeRPCReceive!(Src);
+		
+		@RPCSend!Src
+		void rpcSend(const(ubyte)[] data) {
+			lastSendData = 0~data;
+		}
+		mixin MakeRPCSendTo!(ServerClass!(), Src, Client);
+	}
+	class ServerClass() {
+		@RPC!Src
+		void msg2(S)(int x, Client client) {
+			lastMsg = "msg2: "~x.to!string;
+			msg_send!Src([client], x+1);
+		}
+		mixin MakeRPCReceive!(Src, Client);
+		
+		@RPCSend!Src
+		void rpcSend(Client[] clients, const(ubyte)[] data) {
+			lastSendData = 1~data;
+		}
+		mixin MakeRPCSendTo!(ClientClass!(), Src);
+	}
+	
+	ClientClass!() c = new ClientClass!();
+	ServerClass!() s = new ServerClass!();
+	
+	c.rpcRecv!Src([0,1,0,0,0]);
+	assert(lastMsg == "msg: 1");
+	assert(lastSendData == [0, 0,2,0,0,0]);
+	
+	s.rpcRecv!Src(new Client, [0,3,0,0,0]);
+	assert(lastMsg == "msg2: 3");
+	assert(lastSendData == [1, 0,4,0,0,0]);
 }
 
  
